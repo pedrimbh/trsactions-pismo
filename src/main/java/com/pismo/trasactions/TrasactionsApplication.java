cb8750ac3a632568adb7bcbfd0c34853ad7c5f0e
Joao Pedro <joaopedro@example.com>
2026-04-04 11:57:19 -0300

    Initial commit

---
+++ src/main/java/com/pismo/trasactions/TrasactionsApplication.java
@@ -0,0 +1,13 @@
+package com.pismo.trasactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pismo/trasactions/TrasactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo.trasactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
